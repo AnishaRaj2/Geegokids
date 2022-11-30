125ea72918f01bce9a0e4a31d2d8958225b8b7f7
Tran Thi Thu Trang <thutrang1232003@gmail.com>
2022-11-30 07:33:24 +0200

    test

--- src/SkillsFetchAPI.jsx
+++ src/SkillsFetchAPI.jsx
@@ -1,6 +1,6 @@
 const SkillsFetchAPI = async (token) => {
   const request = await fetch(
-    `${process.env.REACT_APP_API_ENDPOINT}/sportschools/`,
+    `${process.env.REACT_APP_API_ENDPOINT}/sportschoolcategories/`,
     {
       headers: {
         "Content-Type": "application/json",
