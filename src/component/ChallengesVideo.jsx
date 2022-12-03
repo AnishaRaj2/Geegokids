e8e981ef12e2c8261b6bb8882e4a848cfb364473
AnishaRaj2 <mail.anisha.raj@gmail.com>
2022-12-03 22:42:08 +0200

    video test

---
+++ src/component/ChallengesVideo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Video.css";
+
+const ChallengesVideo = (props) => {
+  return (props.trigger) ?
+    (<div className="Video" style={{ display: props.display }}>
+      <video controls>
+        <source src={props.video} type="video/mp4" />
+      </video>
+      <div>
+        <i className="fa-solid fa-xmark" onClick={() => props.setTrigger(false)}></i>
+        <h4>{props.title}</h4>
+        <p>{props.description}</p>
+      </div>
+    </div>) : ""
+};
+
+export default ChallengesVideo;
