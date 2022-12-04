09b0f2fa47d24333ae73473d22e5dc3e0ba1fe3d
AnishaRaj2 <mail.anisha.raj@gmail.com>
2022-12-05 00:30:43 +0200

    toddler video test

---
+++ src/component/ToddlersVideo.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ToddlersVideo = (props) => {
+    return (props.trigger) ?
+    (<div 
+        className="Video" 
+        style={{ display: props.display }}
+     >
+      <video controls>
+        <source 
+            src={props.video} 
+            type="video/mp4" 
+        />
+      </video>
+      <div>
+        <i 
+            className="fa-solid fa-xmark" 
+            onClick={() => props.setTrigger(false)}
+        >
+        </i>
+        <h4>{props.title}</h4>
+        <p>{props.description}</p>
+      </div>
+     </div>
+    ) : ""
+}
+
+export default ToddlersVideo
