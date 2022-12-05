586bc40cf55eac698a92e5b5962e9a5e5e66bc8b
AnishaRaj2 <mail.anisha.raj@gmail.com>
2022-12-05 13:15:29 +0200

    video test

---
+++ src/component/SkillsTasks.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import SkillsTask from "./SkillsTask";
+import "./Tasks.css";
+import "../index.css";
+
+const SkillsTasks = (props) => {
+  return props.trigger ? (
+    <div className="Tasks">
+      <div>
+        <h2>
+          {props.title}
+          <i className="fa-solid fa-circle"></i>
+          <i className="fa-solid fa-circle"></i>
+          <i className="fa-solid fa-circle"></i>
+        </h2>
+        <p>{props.description}</p>
+      </div>
+      <ul>
+        {props.skill.sportschool_task_groups.map((tasks) => {
+          return tasks.sportschool_tasks.map((task) => {
+            return (
+              <li>
+                <SkillsTask
+                  imgUrl={task.thumbnail}
+                  title={task.title}
+                  video={task.video}
+                  description={task.description}
+                />
+              </li>
+            );
+          });
+        })}
+      </ul>
+    </div>
+  ) : (
+    ""
+  );
+};
+
+export default SkillsTasks;
